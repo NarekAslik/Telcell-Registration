c4e4773e6991116bc740ac8ac8cc7c640618071d
NarekAslik <aslikyan01.01@mail.ru>
2023-12-13 17:24:36 +0400

    Adding qr_tool_bar_menu

--- app/src/main/java/com/example/telcellregistration/fragment/FragmentQr.kt
+++ app/src/main/java/com/example/telcellregistration/fragment/FragmentQr.kt
@@ -2,10 +2,13 @@ package com.example.telcellregistration.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import com.example.telcellregistration.R
 import com.example.telcellregistration.adapters.QrCodeAdapter
 import com.example.telcellregistration.databinding.QrFragmentLayoutBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 import com.google.android.material.tabs.TabLayoutMediator
 
 class FragmentQr : Fragment(R.layout.qr_fragment_layout) {
@@ -15,9 +18,11 @@ class FragmentQr : Fragment(R.layout.qr_fragment_layout) {
         QrCodeAdapter(parentFragmentManager, lifecycle)
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = QrFragmentLayoutBinding.bind(view)
+
 
         binding.apply {
             qrLayoutTabLayout.addTab(qrLayoutTabLayout.newTab().setText("Սկանավորել կոդը"))
@@ -33,5 +38,23 @@ class FragmentQr : Fragment(R.layout.qr_fragment_layout) {
                 }
             }.attach()
         }
+        binding.qrLayoutTabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                showMenuItems(tab?.position == 0)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
+    }
+
+    private fun showMenuItems(isVisible: Boolean) {
+        binding.qrLayoutToolBar.menu.children.forEach {
+            it.isVisible = isVisible
+        }
+
     }
 }
